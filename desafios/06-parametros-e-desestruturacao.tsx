ecfbcbb3eeca125d12b9e34154ed214fdc6eea6c
GiovaniFZ <giovanifinazzi@gmail.com>
2024-08-29 12:14:01 -0300

    Desafio 6 - Desestruturação

--- desafios/06-parametros-e-desestruturacao.tsx
+++ desafios/06-parametros-e-desestruturacao.tsx
@@ -1,11 +1,20 @@
-function updateUserRoute(body, params) {
-  updateUserController(body, params)
+function updateUserRoute({body, params}) {
+  updateUserController({
+    data: body,
+    params
+  }
+  )
 }
 
-function updateUserController(data, params) {
-  userRepository.update(data, params)
+// Always receive objects in function params when possible
+function updateUserController({data, params}) {
+  userRepository.update(
+    {data, params}
+  )
 }
 
 const userRepository = {
-  update: (data, params) => {},
+  update: ({data, params}) => {
+    return {data, params}
+  },
 }
