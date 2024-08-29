1030aa920cb7007552b2179f1fabbf6b32985daa
GiovaniFZ <giovanifinazzi@gmail.com>
2024-08-29 11:50:20 -0300

    refactor: Desafio 5 - Separar as condicionais em consts

--- desafios/05-regras-em-condicionais.ts
+++ desafios/05-regras-em-condicionais.ts
@@ -4,15 +4,19 @@ const studentGrade = 10
 const numberOfAbsensesToFailSchool = 100
 const studentNumberOfAbsenses = 109
 
+// Conditionals
+const studentHasGradeLessThanNecessary = studentGrade < necessaryGradeToBeApproved;
+const studentHasTooMuchAbsenses = studentNumberOfAbsenses > numberOfAbsensesToFailSchool
+
 function checkIfStudentPassedTheSchoolYear() {
-  if (studentGrade < necessaryGradeToBeApproved) {
+  if (studentHasGradeLessThanNecessary) {
     return { error: true, message: 'Student was not approved because his grade was below the necessary.'}
   } 
 
-  if(studentNumberOfAbsenses > numberOfAbsensesToFailSchool) {
+  if(studentHasTooMuchAbsenses) {
       return { error: true, message: 'Student was not approved because of his absenses'}
   }
-  
+
   return { error: false, message: 'Student was approved :)'}
 }
 
